8f77310f4e47e287251a402e870898d612233332
ajiikun <45661155+ajiikun@users.noreply.github.com>
2020-03-21 20:26:28 +0700

    Add files via upload

---
+++ nomor1.js
@@ -0,0 +1,15 @@
+Biodata = 
+{
+
+"name"			: "Aji",
+"age" 			: 24,
+"Addres"		: "Yogyakarta, Kotagede",
+"hobbies" 		: [ "maen laptop", "berenang"],
+"is_married" 		: false,
+"list_school" 		: ["UMY", 2013, 2017, "HI"],
+"skills" 		: ["Speaking", "Advanced"],
+"interesting_in_coding" : true
+
+};
+
+JSON.stringify(Biodata);
---
+++ nomor2.js
@@ -0,0 +1,18 @@
+isAcceptedUsername (username)
+
+{
+
+var Regex =  /^[a-z]{6,7}$/
+
+}
+
+return Regex.test (username) ; 
+
+
+isAcceptedPassword (password)
+{
+
+var Regex =  ^[a-zA-Z][@]{9}$/
+}
+
+return Regex.test (username) ; 
